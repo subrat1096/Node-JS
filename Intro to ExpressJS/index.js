49b200b2f9b9f894032cc80eafa11dda1e1910b3
Subrat Kumar Behera <148032888+codexpsk@users.noreply.github.com>
2023-10-21 23:56:30 +0530

    html and json request and send

--- Intro to ExpressJS/index.js
+++ Intro to ExpressJS/index.js
@@ -6,15 +6,18 @@ app.get("/", (req, res) => {
   console.log("data sent from client", req.query);
   console.log("data sent from client", req.query.name);
   res.send(
-    "Welcome " +
-      req.query.name.charAt(0).toUpperCase() +
-      req.query.name.slice(1)
+    `<h1>Welcome <a href="/?name=${req.query.name}">${req.query.name
+      .charAt(0)
+      .toUpperCase()}${req.query.name.slice(1)}</a></h1>`
   );
 });
 
 // respond with "This is about page" when a GET request is made to the aboutpage
 app.get("/about", (req, res) => {
-  res.send("This is about page");
+  res.send({
+    name: "Subrat Kumar Behera",
+    email: "codexpsubrat@gmail.com",
+  });
 });
 
 // start server on port 3000
