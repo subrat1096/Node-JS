9cd0a134abd66c5ba6aaac3e6aff9d54a7377aef
Subrat Kumar Behera <148032888+codexpsk@users.noreply.github.com>
2023-10-22 01:19:44 +0530

    modified HTML page

--- Make HTML page/index.js
+++ Make HTML page/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-
+var options = {
+  extensions: ["htm", "html"],
+};
 const publicPath = path.join(__dirname, "public");
 console.log(publicPath);
-app.use(express.static(publicPath));
+app.use(express.static(publicPath, options));
 // app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
